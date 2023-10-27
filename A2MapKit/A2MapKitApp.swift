77bbc03bf0c2a78745ed7aa5c21cbadcb75bdb80
Marian <alejanma@sheridancollege.ca>
2023-10-27 16:37:43 +0000

    Initial Commit

---
+++ A2MapKit/A2MapKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  A2MapKitApp.swift
+//  A2MapKit
+//
+//  Created by Default User on 10/27/23.
+//
+
+import SwiftUI
+
+@main
+struct A2MapKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ A2MapKit/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  A2MapKit
+//
+//  Created by Default User on 10/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
